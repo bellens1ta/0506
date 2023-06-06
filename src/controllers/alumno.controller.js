9aee27d2e9e8eee82d3708eb07e03183b4ea5a75
bellens1ta <bellfunes91@gmail.com>
2023-06-06 02:20:28 -0300

    segunda subida - alumnos metodos get put post

--- src/controllers/alumno.controller.js
+++ src/controllers/alumno.controller.js
@@ -5,4 +5,62 @@ const getAlumnos = (_, res) => {
     res.status(httpStatusCodes.HTTP_STATUS_OK).json(alumnos)
 }
 
-module.exports = {getAlumnos}
+const getAlmunosByDni =  (req, res) => {
+    const { dni } = req.params
+    const alumnoDni = alumnos.find((a) => a.dni == dni)
+  
+    if (alumnoDni) {
+        res.json(alumnoDni)
+    } else {
+      res.status(httpStatusCodes.HTTP_STATUS_NOT_FOUND).json({ error: 'Dni no encontrado' })
+    }
+  }
+
+  const updateAlumnos = ( req, res)=> { // 
+    const { dni } = req.params
+    const {habilitado, celiaco ,edad} = req.body
+
+    const alumno = alumnos.find((a) => a.dni == dni)
+
+    if (alumno){
+            if (habilitado !== undefined) alumno.habilitado = habilitado
+            if (celiaco !== undefined) alumno.celiaco = celiaco
+            if (edad !== undefined) alumno.edad = edad 
+            res.json({ mensaje: 'Alumno actualizado' , alumno});}
+            else {
+            res.status(httpStatusCodes.HTTP_STATUS_NOT_FOUND).json({ error: 'Alumno no encontrado' });
+            return
+            }
+           
+        }
+  
+
+
+  const addAlumno = (req, res)=> {
+    const { dni, nombre, edad, celiaco = false } = req.body
+    const dniAlumno = alumnos.find((a) => a.dni == dni)
+
+    if (dniAlumno){
+        res.status(httpStatusCodes.HTTP_STATUS_NOT_FOUND).json({ error: 'El dni ingresado ya existe' , dniAlumno});
+    }
+
+    if (dni.toString().length !== 8) {
+         res.status(httpStatusCodes.HTTP_STATUS_NOT_FOUND).json({ error: 'El DNI debe tener 8 numeros' });
+         return
+      }
+      if (edad <= 18 || edad >= 99) {
+        res.status(httpStatusCodes.HTTP_STATUS_NOT_FOUND).json({ error: 'La edad del alumno tiene que ser mayor a 18 y menor a 99', edad });
+        return
+    }
+
+    const nuevoAlumno = { 
+        dni, 
+        nombre, 
+        habilitado: true,
+         celiaco, 
+         edad };
+    alumnos.push(nuevoAlumno);
+    res.status(httpStatusCodes.HTTP_STATUS_CREATED).json({mensaje :"Se agrego el alumno correctamente",alumno:nuevoAlumno});
+  }
+  
+module.exports = {getAlumnos, getAlmunosByDni,updateAlumnos, addAlumno }
--- src/routes/alumno.route.js
+++ src/routes/alumno.route.js
@@ -3,5 +3,8 @@ const controller = require('../controllers/alumno.controller')
 const route = Router()
 
 route.get("/", controller.getAlumnos)
+route.get("/:dni",controller.getAlmunosByDni )
+route.put("/:dni", controller.updateAlumnos)
+route.post("/", controller.addAlumno)
 
 module.exports = route
